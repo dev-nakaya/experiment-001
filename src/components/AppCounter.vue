10f92e5a18a204dd6afc666258b39626c71a7d89
dev-nakaya <dev.nakaya@gmail.com>
2024-05-01 14:06:16 +0900

    add: @nuxt/test-utilsとテスト用コンポーネントの追加

---
+++ src/components/AppCounter.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+const count = ref(0)
+</script>
+
+<template>
+  <div class="m-4 rounded bg-slate-100 p-4">
+    <p class="mx-2">
+      AppCount:
+      <span class="font-bold">
+        {{ count }}
+      </span>
+    </p>
+    <button
+      class="m-2 rounded bg-green-600 px-4 py-2 text-white"
+      @click="count++"
+    >
+      +
+    </button>
+    <button
+      class="m-2 rounded bg-slate-400 px-4 py-2"
+      @click="count--"
+    >
+      -
+    </button>
+  </div>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <p class="m-4 font-bold">
-    index page
-  </p>
+  <div>
+    <h1 class="m-4 text-xl font-bold">
+      index page
+    </h1>
+    <AppCounter />
+  </div>
 </template>
